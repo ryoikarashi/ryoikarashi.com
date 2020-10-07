b31ef51840a9435a94c3cf812b398704f46a3cff
Ryo Ikarashi <ryo.5630@gmail.com>
2020-10-07 21:42:35 +0900

    update data for dev

--- data.development.js
+++ data.development.js
@@ -1,11 +1,11 @@
 const data = {
   data: {
     meta: {
-      title: 'Ryo Ikarashi | WEB Design & Development',
-      desc: '五十嵐諒 / 京都を拠点にウェブサイト/WEBアプリケーションのデザイン・開発を行うフリーランスのエンジニア兼デザイナー',
+      title: 'Ryo Ikarashi',
+      desc: 'Hi, I\'m a freelance software developer based in Kyoto Japan.',
       og: {
-        title: 'Ryo Ikarashi | WEB Design & Development',
-        desc: '五十嵐諒 / 京都を拠点にウェブサイト/WEBアプリケーションのデザイン・開発を行うフリーランスのエンジニア兼デザイナー',
+        title: 'Ryo Ikarashi',
+        desc: 'Hi, I\'m a freelance software developer based in Kyoto Japan.',
         type: 'website',
         url: 'https://ryoikarashi.com',
         image: '../assets/ryoikarashi-ogp.png',
@@ -21,38 +21,11 @@ const data = {
         },
       },
     },
-    footer: {
-      sns: [
-        {
-          url: 'https://twitter.com/ryo_ikarashi',
-          classname: 'twitter'
-        },
-        {
-          url: 'https://facebook.com/ry0ikarash1',
-          classname: 'facebook'
-        },
-        {
-          url: 'https://instagram.com/ryo_ikarashi',
-          classname: 'instagram'
-        },
-      ],
-      copy: 'Copyright©2018 Ryo Ikarashi. All Rights Reserved.',
-    },
     pages: {
       global: {
         title: 'Ryo Ikarashi',
-        sub_title: 'Web design & development',
+        sub_title: '',
         email: 'me@ryoikarashi.com',
-      },
-      top: {
-        desc_ja: [
-          '<span class="db di-ns lh-copy">現在ウェブサイトの</span><span class="db di-ns lh-copy">リニューアルを行っております。</span>',
-          '<span class="db di-ns lh-copy">御用の方は下記メールアドレス宛に</span><span class="db di-ns lh-copy">ご連絡ください。</span>',
-        ],
-        desc_en: [
-          'Currently renewing my website.',
-          'Please feel free to drop me a line :)'
-        ],
       },
     },
   },
