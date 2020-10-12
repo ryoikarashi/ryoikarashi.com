31411662babaf96e550b91c052d77930985a09e9
Ryo Ikarashi <ryo.5630@gmail.com>
2020-10-12 19:40:46 +0900

    tweak

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,15 +1,5 @@
 const nodeExternals = require('webpack-node-externals');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   externals: [nodeExternals()],
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'serviceAccountKey.json', to: `${__dirname}/functions/serviceAccountKey.json`,
-        },
-      ],
-    }),
-  ]
 };
