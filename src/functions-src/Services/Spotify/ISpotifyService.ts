78a2684a183959075ace1c66a0bc0f77d56a5c76
Ryo Ikarashi <ryo.5630@gmail.com>
2020-10-21 01:39:38 +0900

    dead code elimination

--- src/functions-src/Services/Spotify/ISpotifyService.ts
+++ src/functions-src/Services/Spotify/ISpotifyService.ts
@@ -1,9 +1,9 @@
-import {ISpotifyCurrentlyListeningTrackData} from "./ISpotifyCurrentlyListeningTrackData";
 import {Token} from "../../Domains/Token/Token";
 import {AccessToken} from "../../Domains/Token/AccessToken";
+import {Track} from "../../Domains/Track/Track";
 
 export interface ISpotifyService {
     refreshAccessToken(): Promise<AccessToken>;
-    getCurrentlyListeningTrack(accessToken: AccessToken): Promise<ISpotifyCurrentlyListeningTrackData>;
+    getCurrentlyListeningTrack(accessToken: AccessToken): Promise<Track>;
     getToken(): Promise<Token>;
 }
