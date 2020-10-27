8ea71315b0ee78aeaaca4c2b72a319402d461e8c
Ryo Ikarashi <ryo.5630@gmail.com>
2020-10-27 19:10:55 +0900

    fade in/out

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import './index.css';
 import Pusher from 'pusher-js';
 import axios, {AxiosResponse} from 'axios';
 import isEqual from 'lodash.isequal';
+import imagesLoaded from 'imagesloaded';
 import {TrackPlainObj} from "./functions-src/Domains/Track/Track";
 
 let currentlyListening = {};
@@ -47,11 +48,24 @@ const updateBg = (src: string) => {
     }
 };
 
+const toggleContent = () => {
+    const $bg = document.getElementById('bg');
+    const $loading = document.getElementById('loading');
+    const $content = document.getElementById('content');
+    if ($loading && $content && $bg) {
+        console.log('hello');
+        $loading.style.display = 'none';
+        $content.style.display = 'block';
+        $bg.style.display = 'block';
+    }
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
     const [{ data: trackData }, { data: { url } }] = await Promise.all([
         getCurrentlyPlaying(),
         getARandomPhoto(),
     ]);
+
 
     // update background image
     updateBg(url);
@@ -60,6 +74,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     currentlyListening = trackData;
     updateDOM(trackData);
     playClickSound();
+
+    // show main content when the background image is fully loaded
+    imagesLoaded( '#bg', { background: true }, function() {
+        // show main content
+        toggleContent();
+    });
 
     // initialize pusher
     const pusher = new Pusher('f3f5751318b2c7958521', {
