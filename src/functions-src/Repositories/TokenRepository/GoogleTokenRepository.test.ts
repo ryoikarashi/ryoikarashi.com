0837e25c7fa92151a6c29b0c187559bfe1250009
Ryo Ikarashi <ryo.5630@gmail.com>
2020-11-01 01:56:24 +0900

    wip

--- src/functions-src/Repositories/TokenRepository/GoogleTokenRepository.test.ts
+++ src/functions-src/Repositories/TokenRepository/GoogleTokenRepository.test.ts
@@ -1,16 +1,17 @@
+import * as admin from 'firebase-admin';
 import axios from 'axios';
 import { GoogleTokenRepository } from './GoogleTokenRepository';
 import { HTTPTokenResponse, IOAuthConfig } from './ITokenRepository';
 import { Token } from '../../Entities/Token/Token';
 import { RefreshToken } from '../../Entities/Token/RefreshToken';
 import { AccessToken } from '../../Entities/Token/AccessToken';
-import * as admin from 'firebase-admin';
+import { getRootCollectionName } from '../../../utils';
 
 // create mocks
-jest.mock('axios').mock('firebase-admin', () => ({
-    initializeApp: jest.fn(() => ({
-        firestore: jest.fn(),
-    })),
+jest.mock('axios');
+jest.mock('firebase-admin', () => ({
+    initializeApp: jest.fn().mockReturnThis(),
+    firestore: jest.fn(),
 }));
 
 // clear all mocks before each test
@@ -18,51 +19,63 @@ afterEach(() => {
     jest.clearAllMocks();
 });
 
-it('is true', () => {
-    expect(true).toBe(true);
+describe('Test GooglePhotosRepository', () => {
+    const httpTokenResponse: HTTPTokenResponse = {
+        access_token: 'access_token_1',
+        refresh_token: 'refresh_token_1',
+    };
+
+    const token = new Token(
+        AccessToken.of(httpTokenResponse.access_token),
+        RefreshToken.of(httpTokenResponse.refresh_token),
+    );
+
+    // describe('storeAccessTokenAndMaybeRefreshToken', async () => {
+    // });
+
+    describe('getFirstToken', () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        jest.spyOn(admin, 'firestore').mockImplementation((): any => ({
+            collection: jest.fn().mockReturnThis(),
+            doc: jest.fn().mockReturnThis(),
+            get: jest.fn(() => ({
+                data: jest.fn(() => httpTokenResponse),
+                exists: jest.fn().mockResolvedValue(true),
+            })),
+        }));
+        const firestore = admin.initializeApp().firestore();
+        const repository = new GoogleTokenRepository(firestore);
+
+        it('returns a valid token', async () => {
+            await expect(repository.getFirstToken()).resolves.toEqual(token);
+            expect(admin.initializeApp).toHaveBeenCalledTimes(1);
+            expect(firestore.collection).toHaveBeenCalledWith(getRootCollectionName('google_tokens'));
+            expect(firestore.doc).toHaveBeenCalledWith('ryoikarashi-com');
+        });
+    });
+
+    describe('getTokenByAuthorizationCode', () => {
+        const oauthConfig: IOAuthConfig = {
+            authorizationCode: 'authorization_code',
+            clientId: 'client_id',
+            clientSecret: 'client_secret',
+            redirectUri: 'https://example.com/callback',
+        };
+
+        it('returns a valid token', async () => {
+            (axios as jest.Mocked<typeof axios>).post.mockResolvedValueOnce({ data: httpTokenResponse });
+            const repository = new GoogleTokenRepository(admin.firestore());
+            await expect(repository.getTokenByAuthorizationCode(axios, oauthConfig)).resolves.toEqual(token);
+        });
+
+        it('returns an invalid token', async () => {
+            (axios as jest.Mocked<typeof axios>).post.mockRejectedValueOnce(new Error());
+            const repository = new GoogleTokenRepository(admin.firestore());
+            await expect(repository.getTokenByAuthorizationCode(axios, oauthConfig)).rejects.toEqual(new Error());
+        });
+    });
+
+    // describe('refreshToken', () => {
+    // });
 });
-
-//
-// const db = admin.initializeApp().firestore();
-//
-// const repository = new GoogleTokenRepository(db);
-// const httpTokenResponse: HTTPTokenResponse = {
-//    access_token: 'access_token',
-//    refresh_token: 'refresh_token',
-// };
-// const oauthConfig: IOAuthConfig = {
-//    authorizationCode: "authorization_code",
-//    clientId: "client_id",
-//    clientSecret: "client_secret",
-//    redirectUri: "https://example.com/callback",
-// };
-// const token = new Token(
-//     AccessToken.of(httpTokenResponse.access_token),
-//     RefreshToken.of(httpTokenResponse.access_token),
-// )
-
-// describe('storeAccessTokenAndMaybeRefreshToken', () => {
-//
-// });
-//
-// describe('getFirstToken', () => {
-//    const get = jest.fn();
-//    const doc = jest.fn(() => ({ get }));
-//    const collection = jest.spyOn(db, 'collection').mockReturnValueOnce({ doc });
-// });
-//
-// describe('getTokenByAuthorizationCode', () => {
-//    it('returns a valid token', async () => {
-//       (axios as jest.Mocked<typeof axios>).post.mockResolvedValueOnce({ data: httpTokenResponse });
-//       await expect(repository.getTokenByAuthorizationCode(axios, oauthConfig)).toEqual(token);
-//    });
-//
-//    it('returns an invalid token', async () => {
-//       (axios as jest.Mocked<typeof axios>).post.mockRejectedValueOnce(new Error('something went wrong'));
-//       await expect(repository.getTokenByAuthorizationCode(axios, oauthConfig)).toEqual(token);
-//    });
-// });
-//
-// describe('refreshToken', () => {
-//
-// });
