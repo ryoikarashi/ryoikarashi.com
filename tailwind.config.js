58c132651f6615feadb9353168af379339598915
Ryo Ikarashi <ryo.5630@gmail.com>
2020-11-07 02:40:54 +0900

    tweak mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,17 @@ module.exports = {
             backgroundOpacity: {
                 10: '0.1',
             },
+            maxWidth: {
+                '1/2': '50%',
+                '9/10': '90%',
+            },
         },
     },
     variants: {
         backgroundColor: ['hover'],
         textColor: ['hover'],
         transitionProperty: ['responsive', 'hover', 'focus'],
+        maxWidth: ['responsive', 'hover', 'focus'],
     },
     plugins: [],
 };
