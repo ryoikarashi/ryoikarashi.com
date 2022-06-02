437160d85c4e16e0241d3665665c03dd872e5ec8
Ryo Ikarashi <ryo.5630@gmail.com>
2022-06-02 13:30:25 +0900

    declare search limit as an environment variable instead of as hard-coded one

--- src/functions-src/Repositories/GiphyRepository/GiphyRepository.ts
+++ src/functions-src/Repositories/GiphyRepository/GiphyRepository.ts
@@ -1,10 +1,11 @@
+import { GiphyFetch } from '@giphy/js-fetch-api';
 import { IGiphyRepository } from './IGiphyRepository';
 import { Giphy } from '../../Entities/Giphy/Giphy';
 import { Src } from '../../Entities/Giphy/Src';
-import { GiphyFetch } from '@giphy/js-fetch-api';
 
 export class GiphyRepository implements IGiphyRepository {
-    private readonly _limit = 20;
+    private readonly _fallbackSearchLimit = 20;
+    private readonly _fallbackSearchTerm = 'funny kids';
     private readonly _client: GiphyFetch;
 
     constructor(client: GiphyFetch) {
@@ -12,8 +13,11 @@ export class GiphyRepository implements IGiphyRepository {
     }
 
     async getRandom(): Promise<Giphy> {
-        const { data } = await this._client.search(process.env.GIPHY_SEARCH_TERM ?? '', { limit: this._limit });
-        const randomIndex = Math.floor(Math.random() * this._limit);
+        const limit = Number(process.env.GIPHY_SEARCH_LIMIT ?? this._fallbackSearchLimit);
+        const { data } = await this._client.search(process.env.GIPHY_SEARCH_TERM ?? this._fallbackSearchTerm, {
+            limit,
+        });
+        const randomIndex = Math.floor(Math.random() * limit);
         const gif = data[randomIndex];
         const src = gif?.images?.original?.url;
 
