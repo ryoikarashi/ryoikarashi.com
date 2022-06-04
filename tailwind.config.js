49f433a9d1abf07fd51512a0b680c07404045bda
Ryo Ikarashi <ryo.5630@gmail.com>
2022-06-04 21:46:49 +0900

    upgrade modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
         removeDeprecatedGapUtilities: true,
         purgeLayersByDefault: true,
     },
-    purge: ['./src/**/*.{html,js,ts}'],
+    content: ['./src/**/*.{html,js,ts}'],
     theme: {
         extend: {
             colors: {
