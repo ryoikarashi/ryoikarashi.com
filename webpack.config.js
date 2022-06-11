99c469492fd902cbdad8e822b267c1d41633172c
Ryo Ikarashi <ryo.5630@gmail.com>
2022-06-12 00:29:14 +0900

    tweak webpack build settings for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const WebpackBundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
-const publicPath = process.env.NODE_ENV === 'production' ? 'https://ryoikarashi.com' : '/';
+const isProduction = process.env.NODE_ENV === 'production';
+
+const publicPath = isProduction ? 'https://ryoikarashi.com' : '/';
 
 module.exports = {
     entry: './src/index.ts',
+
+    mode: isProduction ? 'production' : 'development',
 
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -16,7 +19,7 @@ module.exports = {
         chunkFilename: '[name].[fullhash].js',
     },
 
-    devtool: 'inline-source-map',
+    devtool: isProduction ? 'nosources-source-map' : 'inline-source-map',
 
     devServer: {
         static: {
@@ -49,25 +52,24 @@ module.exports = {
                     {
                         loader: 'html-loader',
                         options: {
+                            minimize: true,
                             sources: {
                                 list: [
                                     {
                                         tag: 'meta',
                                         attribute: 'content',
                                         type: 'src',
-                                        filter: (_tag, _attribute, attributes, _resourcePath) => {
-                                            return (
-                                                attributes.property === 'og:image' ||
-                                                attributes.name === 'twitter:image'
-                                            );
-                                        },
+                                        filter: (_tag, _attribute, attributes) =>
+                                            attributes.property === 'og:image' || attributes.name === 'twitter:image',
                                     },
                                     {
                                         tag: 'img',
                                         attribute: 'src',
                                         type: 'src',
-                                        filter: (_tag, _attribute, attributes, _resourcePath) => {
-                                            return attributes.src && attributes.src.length;
+                                        filter: (_tag, _attribute, attributes) => {
+                                            return attributes.filter(
+                                                (attr) => attr.name === 'src' && attr.value.length,
+                                            )?.[0];
                                         },
                                     },
                                     {
@@ -97,22 +99,6 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(ogg|mp3|wav|mpe?g)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            publicPath:
-                                process.env.NODE_ENV === 'production'
-                                    ? 'https://ryoikarashi.com/assets/sounds'
-                                    : '/assets/sounds',
-                            outputPath: '/assets/sounds',
-                        },
-                    },
-                ],
-            },
-            {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
             },
@@ -122,38 +108,8 @@ module.exports = {
                 exclude: /node_modules/,
             },
             {
-                test: /\.(jpe?g|png|gif|ico)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[fullhash].[ext]',
-                            limit: '10000',
-                            publicPath:
-                                process.env.NODE_ENV === 'production'
-                                    ? 'https://ryoikarashi.com/assets/images'
-                                    : '/assets/images',
-                            outputPath: '/assets/images',
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2|svg|otf)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            name: '[name].[fullhash].[ext]',
-                            limit: '10000',
-                            publicPath:
-                                process.env.NODE_ENV === 'production'
-                                    ? 'https://ryoikarashi.com/assets/fonts'
-                                    : '/assets/fonts',
-                            outputPath: '/assets/fonts',
-                        },
-                    },
-                ],
+                test: /\.(jpe?g|png|gif|ico|ogg|mp3|wav|mpe?g)$/i,
+                type: 'asset/resource',
             },
         ],
     },
@@ -173,39 +129,38 @@ module.exports = {
             filename: '[name].[fullhash].css',
             chunkFilename: '[id].[fullhash].css',
         }),
-        // new WebpackBundleAnalyzerPlugin(),
-        // new FaviconsWebpackPlugin({
-        //   publicPath: '/',
-        //   // Your source logo
-        //   logo: path.resolve(__dirname, 'src', 'assets', 'favicon.png'),
-        //   // The prefix for all image files (might be a folder or a name)
-        //   prefix: 'assets/icons/',
-        //   // Emit all stats of the generated icons
-        //   emitStats: false,
-        //   // The name of the json containing all favicon information
-        //   statsFilename: 'iconstats-[fullhash].json',
-        //   // Generate a cache file with control hashes and
-        //   // don't rebuild the favicons until those hashes change
-        //   persistentCache: true,
-        //   // Inject the html into the html-webpack-plugin
-        //   inject: true,
-        //   // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
-        //   background: '#fff',
-        //   // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
-        //   title: 'Ryo Ikarashi',
-        //   // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
-        //   icons: {
-        //     android: true,
-        //     appleIcon: true,
-        //     appleStartup: true,
-        //     coast: true,
-        //     favicons: true,
-        //     firefox: true,
-        //     opengraph: true,
-        //     twitter: true,
-        //     yandex: true,
-        //     windows: true,
-        //   },
-        // }),
+        new FaviconsWebpackPlugin({
+            publicPath: '/',
+            // Your source logo
+            logo: path.resolve(__dirname, 'src', 'assets', 'favicon.png'),
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'icons/',
+            // Emit all stats of the generated icons
+            emitStats: false,
+            // The name of the json containing all favicon information
+            statsFilename: 'iconstats-[fullhash].json',
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+            // favicon background color (see https://github.com/haydenbleasel/favicons#usage)
+            background: '#fff',
+            // favicon app title (see https://github.com/haydenbleasel/favicons#usage)
+            title: 'Ryo Ikarashi',
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: true,
+                favicons: true,
+                firefox: true,
+                opengraph: true,
+                twitter: true,
+                yandex: true,
+                windows: true,
+            },
+        }),
     ],
 };
