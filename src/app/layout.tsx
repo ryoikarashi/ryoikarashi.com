e84ac2b159348ccb43f128ba17c29f55580e9e1c
Ryo Ikarashi <ryo.5630@gmail.com>
2023-04-18 05:15:49 +0900

    tweak

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import React, { type ReactNode } from 'react';
 import { Noto_Sans_JP, Josefin_Sans } from 'next/font/google';
 import { Providers } from '@/app/providers';
+import { Analytics } from '@vercel/analytics/react';
 
 export const revalidate = 0;
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`transition duration-300 ${englishFont.className} ${japaneseFont.className} bg-white dark:bg-black`}
       >
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
