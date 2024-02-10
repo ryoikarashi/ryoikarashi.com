7bd5b555bb85a637be7edffd75f4bb853f83e4ac
RYO IKARASHI <ryo.5630@gmail.com>
2024-02-10 18:05:47 +0900

    Fix public image file path

--- src/libs/image.ts
+++ src/libs/image.ts
@@ -1,11 +1,11 @@
-import { join } from 'path';
-import { readFile } from 'fs/promises';
+import { join } from 'node:path';
+import { readFile } from 'node:fs/promises';
 
 export async function getImageBuffer(src: string): Promise<Buffer> {
   if (src.startsWith('http')) {
     const res = await fetch(src);
     return Buffer.from(await res.arrayBuffer());
   } else {
-    return await readFile(join('./public', src));
+    return await readFile(join(__dirname, '../../..', './public', src));
   }
 }
