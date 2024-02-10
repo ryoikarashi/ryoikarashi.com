e7c491702d72432d46b1306372ed43253da33d54
RYO IKARASHI <ryo.5630@gmail.com>
2024-02-10 22:00:20 +0900

    tweak

--- src/components/atoms/Link/index.tsx
+++ src/components/atoms/Link/index.tsx
@@ -29,7 +29,7 @@ export function Link({ noUnderline, ...props }: LinkProps): JSX.Element {
   return (
     <NextLink
       {...props}
-      replace={true}
+      prefetch={true}
       className={twMerge(
         props.className,
         underlineClass,
