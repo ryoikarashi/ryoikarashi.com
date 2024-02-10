dfedfef7162be84ed2fcedec8c691444b7d5f046
RYO IKARASHI <ryo.5630@gmail.com>
2024-02-10 19:40:04 +0900

    tweak 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Photo } from '@/components/organisms';
 import { Link, Text } from '@/components/atoms';
 
 export default function NotFound(): JSX.Element {
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
       <Link href={'/'} noUnderline={true} className='block'>
-        <div className='h-full w-full'>
-          <Photo.Frame>
-            <Photo.Image />
-          </Photo.Frame>
-        </div>
-        <div className='mt-3 text-right'>
-          <Text size='md'>404 NOT FOUND</Text>
-        </div>
+        <Text size='md'>404 Not Found</Text>
       </Link>
     </div>
   );
