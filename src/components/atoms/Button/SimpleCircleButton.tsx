08c1bbe92c07fff6ef8f2616818af13a6fbf7ba2
RYO IKARASHI <ryo.5630@gmail.com>
2024-02-16 17:37:22 +0900

    tweak border width of theme-switcher button

--- src/components/atoms/Button/SimpleCircleButton.tsx
+++ src/components/atoms/Button/SimpleCircleButton.tsx
@@ -12,7 +12,7 @@ export function SimpleCircleButton(
       {...props}
       className={twMerge(
         props.className,
-        'p-6 outline-none after:block after:h-[10px] after:w-[10px] after:rounded-full after:border after:border-black after:bg-white after:duration-200 hover:after:bg-black after:dark:border-white after:dark:bg-black hover:after:dark:bg-white'
+        'p-6 outline-none after:block after:h-[6px] after:w-[6px] after:rounded-full after:border after:border-black after:bg-white after:duration-200 hover:after:bg-black after:dark:border-white after:dark:bg-black hover:after:dark:bg-white'
       )}
     ></button>
   );
