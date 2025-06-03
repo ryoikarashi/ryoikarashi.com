909e6b47ea5d7d8c450c42859f5f774d6c91e2e5
RYO IKARASHI <ryo.5630@gmail.com>
2025-06-03 16:56:09 +0900

    fix error handling on refreshing token

--- src/app/api/photos/random/route.ts
+++ src/app/api/photos/random/route.ts
@@ -1,46 +1,48 @@
-import axios from 'axios';
+// import axios from 'axios';
 import { NextResponse } from 'next/server';
-import { GooglePhotoService } from '@/packages/ryoikarashi/application/Photo/GooglePhotoService';
-import { GooglePhotosRepository } from '@/packages/ryoikarashi/infrastructure/repositories/PhotoRepository/GooglePhotosRepository';
-import { TokenService } from '@/packages/ryoikarashi/application/Token/TokenService';
-import { GoogleTokenRepository } from '@/packages/ryoikarashi/infrastructure/repositories/TokenRepository/GoogleTokenRepository';
-import { FirebaseService } from '@/packages/ryoikarashi/application/Firebase/FirebaseService';
-import { type IOAuthConfig } from '@/packages/ryoikarashi/infrastructure/repositories/TokenRepository/ITokenRepository';
-import { AlbumId } from '@/packages/ryoikarashi/domain/models/Photo/ValueObjects';
+// import { GooglePhotoService } from '@/packages/ryoikarashi/application/Photo/GooglePhotoService';
+// import { GooglePhotosRepository } from '@/packages/ryoikarashi/infrastructure/repositories/PhotoRepository/GooglePhotosRepository';
+// import { TokenService } from '@/packages/ryoikarashi/application/Token/TokenService';
+// import { GoogleTokenRepository } from '@/packages/ryoikarashi/infrastructure/repositories/TokenRepository/GoogleTokenRepository';
+// import { FirebaseService } from '@/packages/ryoikarashi/application/Firebase/FirebaseService';
+// import { type IOAuthConfig } from '@/packages/ryoikarashi/infrastructure/repositories/TokenRepository/ITokenRepository';
+// import { AlbumId } from '@/packages/ryoikarashi/domain/models/Photo/ValueObjects';
 import { Photo } from '@/packages/ryoikarashi/domain/models';
 
-const db = new FirebaseService({
-  databaseURL: process.env.FIRESTORE_DB_URL ?? '',
-  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? '',
-  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL ?? '',
-  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID ?? '',
-}).init();
+// const db = new FirebaseService({
+//   databaseURL: process.env.FIRESTORE_DB_URL ?? '',
+//   privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? '',
+//   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL ?? '',
+//   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID ?? '',
+// }).init();
 
-const googleOAuthConfig: IOAuthConfig = {
-  clientId: process.env.GOOGLE_API_CLIENT_ID ?? '',
-  clientSecret: process.env.GOOGLE_API_CLIENT_SECRET ?? '',
-  authorizationCode: process.env.GOOGLE_API_AUTH_CODE ?? '',
-  redirectUri: process.env.GOOGLE_API_REDIRECT_URI ?? '',
-};
+// const googleOAuthConfig: IOAuthConfig = {
+//   clientId: process.env.GOOGLE_API_CLIENT_ID ?? '',
+//   clientSecret: process.env.GOOGLE_API_CLIENT_SECRET ?? '',
+//   authorizationCode: process.env.GOOGLE_API_AUTH_CODE ?? '',
+//   redirectUri: process.env.GOOGLE_API_REDIRECT_URI ?? '',
+// };
 
 export async function GET(): Promise<NextResponse> {
-  try {
-    const googlePhotos = new GooglePhotoService(
-      new GooglePhotosRepository(axios),
-      new TokenService(
-        axios,
-        new GoogleTokenRepository(db, 'google_tokens', 'ryoikarashi-com'),
-        googleOAuthConfig
-      )
-    );
-    const photo = await googlePhotos.getARandomPhotoFromAlbum(
-      AlbumId.of(process.env.GOOGLE_PHOTOS_ALBUM_ID ?? '')
-    );
-    return NextResponse.json(photo.toPlainObj());
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json(Photo.DEFAULT_PLAIN_OBJ);
-  }
+  return NextResponse.json(Photo.DEFAULT_PLAIN_OBJ);
+
+  // try {
+  //   const googlePhotos = new GooglePhotoService(
+  //     new GooglePhotosRepository(axios),
+  //     new TokenService(
+  //       axios,
+  //       new GoogleTokenRepository(db, 'google_tokens', 'ryoikarashi-com'),
+  //       googleOAuthConfig
+  //     )
+  //   );
+  //   const photo = await googlePhotos.getARandomPhotoFromAlbum(
+  //     AlbumId.of(process.env.GOOGLE_PHOTOS_ALBUM_ID ?? '')
+  //   );
+  //   return NextResponse.json(photo.toPlainObj());
+  // } catch (err) {
+  //   console.error(err);
+  //   return NextResponse.json(Photo.DEFAULT_PLAIN_OBJ);
+  // }
 }
 
 export const dynamic = 'force-dynamic';
